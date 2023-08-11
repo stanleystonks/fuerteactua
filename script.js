f036988e3a8edf68e59912531f7ea3a4f541853f
stanleystonks <stoyan.tonchev80@gmail.com>
2023-08-11 14:32:31 +0100

    Final changes

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
 const langButton = document.querySelector('.lang-icon');
 const elementsToModify = document.querySelectorAll('.element-to-modify');
 const html = document.querySelector('html');
+const languages = ['es', 'en', 'de'];
+let currentLanguageIndex = 0;
 
 elementsToModify.forEach(element => {
     element.dataset.esText = element.innerText;
 });
 
+function updateLanguage() {
+    elementsToModify.forEach(element => {
+        const langKey = `${languages[currentLanguageIndex]}Text`;
+        element.innerText = element.dataset[langKey];
+    });
+    html.lang = languages[currentLanguageIndex];
+}
+
 function changeLanguage() {
-    if (html.lang === 'es') {
-        elementsToModify.forEach(element => {
-            const langKey = `enText`;
-            element.innerText = element.dataset[langKey];
-            html.lang = 'en';
-        });
-    } else if (html.lang === 'en') {
-        elementsToModify.forEach(element => {
-            const langKey = `deText`;
-            element.innerText = element.dataset[langKey];
-            html.lang = 'de';
-        });
-    } else {
-        elementsToModify.forEach(element => {
-            const langKey = `esText`;
-            element.innerText = element.dataset[langKey];
-            html.lang = 'es';
-        });
-    }
+    currentLanguageIndex = (currentLanguageIndex + 1) % languages.length;
+    console.log(currentLanguageIndex);
+    updateLanguage();
 }
 
 langButton.addEventListener('click', changeLanguage);
